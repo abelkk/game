1f607f122cdb276c65b54ba43029574a2084f766
Abel kemboi <74068601+abelkk@users.noreply.github.com>
2021-05-08 15:30:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+print('Welcome to my first game!')
+name = input('What is your name? ')
+print('Hello' + name)
+age=input("what is your age? ")
+    game =input('Do you want to play this game?  ')
+    if game=='yes':
+        print('lets play')
+        if game=='no' :
+        north_or_south = input('do you want to go north_or_south? ')
+        if  north_or_south== "left":
+            print('Nice..follow me to the lake')
+           swim_or_go around = input("Do you want to swim_or_go around? ")
+            if swim_or_go around =='go around':
+                print("you managed to go round the lake")
+                if swim_or_go around == 'swim' :
+                    print("You swam across the lake however you were bitten by a crocodile in the act")
+                    print('thank you for playing my game!')
+                    opinion = input('what is your opinion? ')
+                    print('THANK YOU FOR YOUR OPINION')
